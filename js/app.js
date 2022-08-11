984b18f7cceffaa553e7152e30edc5b815d371fb
Relcode <princefana7@gmail.com>
2022-08-11 20:21:48 +0200

    + add pieces. move pawns, enable 2 steps on start

--- js/app.js
+++ js/app.js
@@ -1,30 +1,72 @@
-//here is where all the game play logic will be
-const allSquares = document.querySelectorAll('.block');
-for(const currentSquare of allSquares){
-    currentSquare.addEventListener('click',function(){
-        if(currentSquare.classList.contains('playable')){
-            let src = selectedPiece;
-            selectedPiece.remove();
-            removeClassPlayable()
-            currentSquare.append(src);
-        }
-    })
-}
-const allPieces = document.querySelectorAll('.piece');
+var squares = document.querySelectorAll('.block')
+var turn = 'white';
 var selectedPiece;
-for(const currentPiece of allPieces){
-    currentPiece.addEventListener('click',function(){
-        removeClassPlayable()
-        let piece = this.dataset.piece;
-        let side = this.dataset.side;
-        let point = this.parentElement.id;
-        selectedPiece = this;
-        switch (piece) {
+var selectedBlock;
+var side;
+var attack;
+
+function availablePawnMoves(selectedSquare){
+    let boardPins = selectedSquare.id.split('-')//refers to where the selected sqaure is
+    removePlayableSquares();
+    if(selectedSquare.childNodes[1].dataset.side == 'white'){
+        let lw = document.getElementById((Number(boardPins[0]) - 1) +'-'+(Number(boardPins[1]) - 1));
+        let cf = document.getElementById((Number(boardPins[0]) - 1) +'-'+(Number(boardPins[1]) - 0));
+        let cf2 = Number(boardPins[0]) == 6 ? document.getElementById((Number(boardPins[0]) - 2) +'-'+(Number(boardPins[1]) - 0)) : '';
+        let rw = document.getElementById((Number(boardPins[0]) - 1) +'-'+(Number(boardPins[1]) + 1));
+        playablePawnMoves(lw,cf,cf2,rw)
+    }else{
+        let lw = document.getElementById((Number(boardPins[0]) + 1) +'-'+(Number(boardPins[1]) - 1));
+        let cf = document.getElementById((Number(boardPins[0]) + 1) +'-'+(Number(boardPins[1]) - 0));
+        let cf2 = Number(boardPins[0]) == 1 ? document.getElementById((Number(boardPins[0]) + 2) +'-'+(Number(boardPins[1]) - 0)) : '';
+        let rw = document.getElementById((Number(boardPins[0]) + 1) +'-'+(Number(boardPins[1]) + 1));
+        playablePawnMoves(lw,cf,cf2,rw)
+    }
+}
+
+function playablePawnMoves(lw,cf,cf2,rw){
+    if(lw != null){//lw refers to the spot left-wide of the selectedSquare, we check if is not null
+        if(lw.childNodes.length > 1 && lw.childNodes[1].dataset.side != turn){
+            lw.classList.add('playable')
+        }
+    }
+    if(cf.childNodes.length == 1){
+        cf.classList.add('playable')
+    }
+    if(cf2 != ''){
+        cf2.classList.add('playable')
+    }
+    if(rw != null){//lw refers to the spot left-wide of the selectedSquare, we check if is not null
+        if(rw.childNodes.length > 1 && rw.childNodes[1].dataset.side != turn){
+            rw.classList.add('playable')
+        }
+    }
+    selectedPiece = undefined;
+}
+
+for(const square of squares){
+    square.addEventListener('click',() => {
+        if(square.classList.contains('playable')){
+            let src = selectedBlock.childNodes[1];
+            if(square.childNodes.length > 1){
+                square.childNodes[1].remove()
+            }
+            square.append(src)
+            removePlayableSquares()
+            //this will execute when a piece can be moved here
+        }else{//we are selecting a piece to move
+            //if the clicked square has a piece to be moved
+            if(square.childNodes.length > 1){
+                selectedBlock = square;
+                selectedPiece = square.childNodes[1].dataset.piece;
+            }
+        }
+        switch (selectedPiece) {
             case 'pawn':
-                pawnAvailableMoves(side,point)
+                availablePawnMoves(square)
                 break;
             case 'knight':
-                knighAvailableMoves(side,point)
+                console.log('f')
+                availableKnightMoves(selectedPiece)
                 break;
             default:
                 break;
@@ -32,56 +74,9 @@ for(const currentPiece of allPieces){
     })
 }
 
-
-function pawnAvailableMoves(side, point){
-
-    const points = point.split('-');
-    /* lw == left-wide, cf == center-forward, rw == right-wide */
-    var lw;
-    var cf;
-    var cf2;//applicable to first move since the pawn can jump 2 squares at the beginning
-    var rw;
-    var attack;
-    if(side == 'white'){
-        attack = 'black';
-        lw = document.getElementById((Number(points[0]) - 1) + '-' + (Number(points[1]) - 1));
-        cf = document.getElementById((Number(points[0]) - 1) + '-' + (Number(points[1]) - 0));
-        if(Number(point[0]) == 6){
-            cf2 = document.getElementById((Number(points[0]) - 2) + '-' + (Number(points[1]) - 0));
-        }
-        rw = document.getElementById((Number(points[0]) - 1) + '-' + (Number(points[1]) + 1));
-    }else if(side == 'black'){
-        attack = 'white';
-        lw = document.getElementById((Number(points[0]) + 1) + '-' + (Number(points[1]) - 1));
-        cf = document.getElementById((Number(points[0]) + 1) + '-' + (Number(points[1]) - 0));
-        if(Number(point[0]) == 1){
-            cf2 = document.getElementById((Number(points[0]) + 2) + '-' + (Number(points[1]) - 0));
-        }
-        rw = document.getElementById((Number(points[0]) + 1) + '-' + (Number(points[1]) + 1));
-    }
-    // check if advanceable squares are empty by testing if they exist or don't have 
-    if(cf != null && cf.childNodes.length == 1){
-        cf.classList.add('playable');
-    }
-    if(cf2 != null && cf2.childNodes.length == 1){
-        cf2.classList.add('playable');
-    }
-    if(lw != null && lw.childNodes.length > 1){
-        if(lw.childNodes[1].dataset.side == attack){
-            lw.classList.add('playable')
-        }
-    }
-    if(rw != null && rw.childNodes.length > 1){
-        if(rw.childNodes[1].dataset.side == attack){
-            rw.classList.add('playable')
-        }
-    }
-}
-
-function removeClassPlayable(){
-    const playables = document.querySelectorAll('.playable');
+function removePlayableSquares(){
+    let playables = document.querySelectorAll('.playable');
     for (let i = 0; i < playables.length; i++) {
         playables[i].classList.remove('playable')
     }
-    return;
 }
